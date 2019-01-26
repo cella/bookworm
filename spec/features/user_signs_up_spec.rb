34aa17b181064d56512177425f12ce59a0e373c9
Cella Sum <cella.sum@gmail.com>
2019-01-25 19:14:29 -0500

    Test if user signs up unsuccessfully

--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -15,4 +15,19 @@ RSpec.feature "User signs up" do
     expect(page).to have_current_path("/")
     expect(page).to have_content "Welcome! You have signed up successfully"
   end
+
+  scenario "unsuccessfully" do
+    visit "/"
+    click_link "Sign up"
+    expect(page).to have_current_path("/users/sign_up")
+
+    fill_in "Email", with: "test@test.com"
+    fill_in "Password", with: "test"
+    fill_in "Password confirmation", with: "test"
+
+    click_button "Sign up"
+
+    expect(page).to have_current_path("/users")
+    expect(page).to have_css("#error_explanation")
+  end
 end
